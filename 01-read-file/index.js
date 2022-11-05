77ac87366b15be422c1b5d68296f72a5c07e0502
Hanna Zalatarevich <a.zoloto@mail.ru>
2022-11-05 14:46:30 +0300

    feat: use stream in 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
 const path = require('path');
-fs.readFile(
+/*fs.readFile(
     path.join(__dirname, 'text.txt'),
     'utf-8',
     (err, data) => {
         if (err) throw err;
         console.log(data);
     }
-);
+);*/
+const readableStream = fs.createReadStream( path.join(__dirname, 'text.txt'));
+let data = '';
+readableStream.on('data', chunk => data += chunk);
+readableStream.on('end', () => console.log(data));
+readableStream.on('error', error => console.log('Error', error.message));
